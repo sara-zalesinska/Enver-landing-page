155aec59b024a9cc0ab53d55b0388d2a3ddb16f8
Sara Zalesinska <sara.zalesinska@outlook.com>
2023-07-18 16:28:14 +0200

    created aliases

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,11 @@
-export default {};
+import { fileURLToPath, URL } from 'node:url';
+
+export default {
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./styles/', import.meta.url)),
+      '@scripts': fileURLToPath(new URL('./scripts/', import.meta.url)),
+    },
+  },
+};
