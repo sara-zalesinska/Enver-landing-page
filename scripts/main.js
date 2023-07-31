4e4d34b0fa21ed0545ea1eeb8edfae9a4fdc85c5
Sara Zalesinska <sara.zalesinska@outlook.com>
2023-07-31 17:39:42 +0200

    created portfolio section

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
 import '../styles/main.scss';
+import Swiper from 'swiper';
+import 'swiper/css';
+
+import { Navigation } from 'swiper/modules';
+
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    slidesPerView: 1,
+    modules: [Navigation],
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
+
