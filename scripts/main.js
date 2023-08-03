df9ee79646566263a6d8ea752d97df6feabd4b06
Sara Zalesinska <sara.zalesinska@outlook.com>
2023-08-03 14:37:57 +0200

    added swiper and styled portfolio section

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,31 @@ import '../styles/main.scss';
 import Swiper from 'swiper';
 import 'swiper/css';
 
-import { Navigation } from 'swiper/modules';
+import { Navigation, Pagination } from 'swiper/modules';
+
 
 const swiper = new Swiper('.swiper', {
     speed: 400,
     spaceBetween: 100,
     slidesPerView: 1,
-    modules: [Navigation],
+    breakpoints: {
+        768: {
+            // slidesPerView: 3,
+            // spaceBetween: 20,
+
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        }
+    },
+    modules: [Navigation, Pagination],
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
       },
   });
 
