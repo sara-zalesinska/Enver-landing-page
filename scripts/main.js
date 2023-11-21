f89b13487df6086585c8112322b2b4580c96f989
Sara Zalesinska <sara.zalesinska@outlook.com>
2023-11-21 20:25:07 +0100

    refactored the code

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,12 @@ import '../styles/main.scss';
 
 import { setupCarousel } from './components/carousel';
 import { setupVideoPlayer } from './components/videoplayer';
-import { setupHamburgerMenu } from './components/hamburger';
+import { setupNavigation } from './components/navigation';
 
 document.addEventListener('DOMContentLoaded', () => {
   setupCarousel();
   setupVideoPlayer();
-  setupHamburgerMenu();
+  setupNavigation();
 })
 
 
